29a20bff42467268633fccec431057ac749e3dbc
Mateus321 <mateuspereirafernandes@gmail.com>
2024-03-19 18:18:25 -0300

    Melhora da apresentação do codigo

--- main.cpp
+++ main.cpp
@@ -2,28 +2,20 @@
 #include <fstream>
 using namespace std;
 
-int main (int argc, char **argv) 
+void transposto (Grafo *grafo)
 {
-    ifstream in ("input.txt");
-    Grafo *grafo = new Grafo (in);
-    grafo->imprime ();  
+    Grafo *grafoT = grafo->grafoTransposto();
+    grafoT->imprime();
+}
 
-    cout << endl; 
+void grafoNaoDirecionado (Grafo *grafo)
+{
+    Grafo *grafoND = grafo->grafoNaoDirecionado();
+    grafoND->imprime();
+}
 
-    //Ex1: criar e imprimir grafo transposto.
-
-    //Grafo *grafoT = grafo->grafoTransposto();
-    //grafoT->imprime();
-
-    //Ex2: Criar e imprimir um grafo não orientado.
-
-
-    //Grafo *grafoND = grafo->grafoNaoDirecionado();
-    //grafoND->imprime();
-    
-    //Ex3: Ler e inserir nova aresta, verificando se ela já existe.
-    int i, j;  
-    /*cin >> i >> j;
+void insereAresta (Grafo *grafo, int i, int j, int peso)
+{
     if (grafo->existeAresta(i, j))
     {
         cout << "Aresta já existe" << endl;
@@ -35,14 +27,11 @@ int main (int argc, char **argv)
         cin >> peso;
         grafo->insereAresta(i, j, peso);
         grafo->imprime();
-    }*/
-    
-    cout << endl;
-    //grafo->imprime();
-   
-    //Ex4: Ler e remover uma aresta, verificando se ela já existe.
-    
-    /*cin >> i >> j;
+    }
+}
+
+void removerAresta (Grafo *grafo, int i, int j)
+{
     if (grafo->existeAresta(i, j))
     {
         grafo->retiraAresta(i, j);
@@ -53,58 +42,114 @@ int main (int argc, char **argv)
     {
         cout << "Aresta não existe" << endl;
     }
-    cout << endl;*/
+}
+
+void listaAdjacente (Grafo *grafo, int i)
+{
+    vector<int> lista = grafo->listaAdj(i);
+    for (int i=0; i < lista.size(); i++)
+        cout << lista.at(i) << " ";
+    cout << endl;
+}
+
+void caminhoEuler (Grafo *grafo)
+{
+    if (grafo->existeCaminhoEuler())
+        cout << "Existe caminho de Euler" << endl;
+    else
+        cout << "Não existe caminho de Euler" << endl;
+}
+
+void cicloEuleriano (Grafo *grafo)
+{
+    if (grafo->existeCicloEuler())
+        cout << "Existe ciclo de Euler" << endl;
+    else
+        cout << "Não existe ciclo de Euler" << endl;
+}
+
+void direcionado (Grafo *grafo)
+{
+    if (grafo->direcionado())
+        cout << "Grafo direcionado" << endl;
+    else
+        cout << "Grafo não direcionado" << endl;
+}
+
+void autoloop (Grafo *grafo)
+{
+    if (grafo->autoloop())
+        cout << "Existe autoloop" << endl;
+    else
+        cout << "Não existe autoloop" << endl;
+}
+
+void completo (Grafo *grafo)
+{
+    if (grafo->completo())
+        cout << "Grafo completo" << endl;
+    else
+        cout << "Grafo não completo" << endl;
+}
+
+int main (int argc, char **argv) 
+{
+    ifstream in ("input.txt");
+    Grafo *grafo = new Grafo (in);
+    grafo->imprime ();  
+
+    cout << endl; 
+
+    //Ex1: criar e imprimir grafo transposto.
+
+    //transposto(grafo);
+
+    //Ex2: Criar e imprimir um grafo não orientado.
+
+    //grafoNaoDirecionado(grafo);
+    
+    //Ex3: Ler e inserir nova aresta, verificando se ela já existe.
+    int i, j;  
+    /*cin >> i >> j;
+    insereAresta(grafo, i, j, 0);
+    
+    cout << endl;
+    //grafo->imprime();
+   
+    //Ex4: Ler e remover uma aresta, verificando se ela já existe.
+    
+    cin >> i >> j;
+    removerAresta(grafo, 0, 1);
     
 
     //Ex5: Imprimir uma lista adjacente de um determinado vértices.
 
-    /*vector<int> lista = grafo->listaAdj(0);
+    listaAdjacente(grafo, 0);
+    
+    cout << endl;
 
-    for (int i=0; i < lista.size(); i++)
-        cout << lista.at(i) << " ";
+    //Ex6: Verificando se existe caminho de euler
+    caminhoEuler(grafo);
     
-    cout << endl;*/
+    
+    //Ex7: Verificando se existe ciclo euleriano
 
-    //Verificando se existe caminho de euler
-    /*if (grafo->existeCaminhoEuler())
-        cout << "Existe caminho de Euler" << endl;
-    
-    else
-        cout << "Não existe caminho de Euler" << endl;
-    
-    
-    //Verificando se existe ciclo euleriano
-    if (grafo->existeCicloEuler())
-        cout << "Existe ciclo de Euler" << endl;
-    
-    else
-        cout << "Não existe ciclo de Euler" << endl;
+    cicloEuleriano(grafo);
 
-*/
-    //Verificar se é direcionado
-/*
-    if (grafo->direcionado())
-        cout << "Grafo direcionado" << endl;
-    
-    else
-        cout << "Grafo não direcionado" << endl;
-*/
-    //Verificar se existe autoloop
-/*
-    if (grafo->autoloop())
-        cout << "Existe autoloop" << endl;
-    
-    else
-        cout << "Não existe autoloop" << endl;
-*/
-    //Verificar se é completo
 
-    if (grafo->completo())
-        cout << "Grafo completo" << endl;
-    
-    else
-        cout << "Grafo não completo" << endl;
+    //Ex7: Verificar se é direcionado
 
+    direcionado(grafo);
+
+    //Ex8: Verificar se existe autoloop
+
+    autoloop(grafo);
+
+    //Ex9: Verificar se é completo
+
+    completo(grafo);
+    */
+    
     
 
 }
